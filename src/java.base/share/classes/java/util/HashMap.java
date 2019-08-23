b0b0e2c12fe9c6ea62f15b72785ce6b820850feb
Michael Ernst <mernst@cs.washington.edu>
2019-08-23 06:59:48 -0700

    Add missing import statement

--- src/java.base/share/classes/java/util/HashMap.java
+++ src/java.base/share/classes/java/util/HashMap.java
@@ -34,6 +34,7 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.dataflow.qual.Pure;
 import org.checkerframework.dataflow.qual.SideEffectFree;
 import org.checkerframework.framework.qual.AnnotatedFor;
+import org.checkerframework.framework.qual.CFComment;
 
 import java.io.IOException;
 import java.io.InvalidObjectException;
