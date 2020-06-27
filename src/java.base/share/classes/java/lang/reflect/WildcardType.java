b62dc442c6ff1b002dae04a26d33a78fc1fc3a3c
Priti Chattopadhyay <35490584+PRITI1999@users.noreply.github.com>
2020-06-27 10:05:18 +0530

    Fix import

--- src/java.base/share/classes/java/lang/reflect/WildcardType.java
+++ src/java.base/share/classes/java/lang/reflect/WildcardType.java
@@ -24,7 +24,7 @@
  */
 
 package java.lang.reflect;
-import org.checkerframework.checker.value.MinLen;
+import org.checkerframework.common.value.MinLen;
 
 /**
  * WildcardType represents a wildcard type expression, such as
