15f75856b292726641217ea31b42062352c778b4
Michael Ernst <mernst@cs.washington.edu>
2022-05-09 09:07:49 -0700

    Annotate `StringBuilder.toString` as `@PolyRegex`

--- src/java.base/share/classes/java/lang/StringBuilder.java
+++ src/java.base/share/classes/java/lang/StringBuilder.java
@@ -31,6 +31,7 @@ import org.checkerframework.checker.index.qual.LTLengthOf;
 import org.checkerframework.checker.index.qual.NonNegative;
 import org.checkerframework.checker.lock.qual.GuardSatisfied;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.checkerframework.checker.regex.qual.PolyRegex;
 import org.checkerframework.dataflow.qual.Pure;
 import org.checkerframework.dataflow.qual.SideEffectFree;
 import org.checkerframework.framework.qual.AnnotatedFor;
@@ -459,7 +460,7 @@ public final class StringBuilder
     @SideEffectFree
     @Override
     @HotSpotIntrinsicCandidate
-    public String toString(@GuardSatisfied StringBuilder this) {
+    public @PolyRegex String toString(@GuardSatisfied @PolyRegex StringBuilder this) {
         // Create a copy, don't share the array
         return isLatin1() ? StringLatin1.newString(value, 0, count)
                           : StringUTF16.newString(value, 0, count);
