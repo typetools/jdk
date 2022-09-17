a10f2e039d9dcb7f75d3503abd514fd9538981fd
Michael Ernst <mernst@cs.washington.edu>
2022-09-17 11:55:44 -0700

    Add `import` statement

--- src/java.base/share/classes/java/io/StreamTokenizer.java
+++ src/java.base/share/classes/java/io/StreamTokenizer.java
@@ -29,6 +29,7 @@ import org.checkerframework.checker.index.qual.NonNegative;
 import org.checkerframework.checker.interning.qual.UsesObjectEquals;
 import org.checkerframework.checker.lock.qual.GuardSatisfied;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.checkerframework.common.value.qual.IntRange;
 import org.checkerframework.common.value.qual.IntVal;
 import org.checkerframework.dataflow.qual.SideEffectFree;
 import org.checkerframework.framework.qual.AnnotatedFor;
