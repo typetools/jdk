6a5df67940d7533cdda151e886fcf93269416ab4
iywang2016 <ivyk2016@gmail.com>
2025-05-15 03:02:25 -0700

    Removed PolyConfidential receiver annotation for toString

--- src/java.base/share/classes/java/lang/Object.java
+++ src/java.base/share/classes/java/lang/Object.java
@@ -285,7 +285,7 @@ public class Object {
     "that differs according to ==, and @Deterministic requires that the results of",
     "two calls of the method are ==."})
     @SideEffectFree
-    public @Confidential String toString(@PolyConfidential @GuardSatisfied Object this) {
+    public @Confidential String toString(@GuardSatisfied Object this) {
         return getClass().getName() + "@" + Integer.toHexString(hashCode());
     }
 
