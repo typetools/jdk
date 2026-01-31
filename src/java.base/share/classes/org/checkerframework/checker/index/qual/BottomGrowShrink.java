ca6e3e6da0fc46a47f62656a6bf619ffb74d180a
Michael Ernst <mernst@cs.washington.edu>
2026-01-31 10:31:58 -0800

    Rename Shrinkable to CanShrink, to accommodate a future checker

--- src/java.base/share/classes/org/checkerframework/checker/index/qual/BottomGrowShrink.java
+++ src/java.base/share/classes/org/checkerframework/checker/index/qual/BottomGrowShrink.java
@@ -16,5 +16,5 @@ import org.checkerframework.framework.qual.SubtypeOf;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
-@SubtypeOf({GrowOnly.class, UncheckedShrinkable.class})
+@SubtypeOf({GrowOnly.class, UncheckedCanShrink.class})
 public @interface BottomGrowShrink {}
