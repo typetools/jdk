ebe283f490c3cba11cf3525c943947baaab01958
Michael Ernst <mernst@cs.washington.edu>
2026-04-21 20:49:55 -0700

    Make `close()` be side-effect-free

--- src/java.base/share/classes/java/lang/AutoCloseable.java
+++ src/java.base/share/classes/java/lang/AutoCloseable.java
@@ -26,6 +26,7 @@
 package java.lang;
 
 import org.checkerframework.checker.lock.qual.GuardSatisfied;
+import org.checkerframework.dataflow.qual.SideEffectFree;
 import org.checkerframework.framework.qual.AnnotatedFor;
 import org.checkerframework.framework.qual.CFComment;
 
@@ -104,5 +105,6 @@ public interface AutoCloseable {
      *
      * @throws Exception if this resource cannot be closed
      */
+    @SideEffectFree
     void close(@GuardSatisfied AutoCloseable this) throws Exception;
 }
