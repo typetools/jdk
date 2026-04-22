aa32a00c7fb6357e58921851bb2d7e7d628ec388
Sanjay Malakar <iamsanjaymalakar@gmail.com>
2026-04-22 11:10:40 -0700

    dummy commit to make the CI rerun.

--- src/java.base/share/classes/java/lang/Throwable.java
+++ src/java.base/share/classes/java/lang/Throwable.java
@@ -765,6 +765,7 @@ public @UsesObjectEquals class Throwable implements Serializable {
      * @param s {@code PrintWriter} to use for output
      * @since   1.1
      */
+    @SideEffectFree
     public void printStackTrace(PrintWriter s) {
         printStackTrace(new WrappedPrintWriter(s));
     }
